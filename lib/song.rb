cc518f2910bade05d689d948e4df4b6f73e55728
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-30 06:22:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,13 @@
 class Song
  @@count = 0
  @@artists = []
+ @@genres = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @@artists << artist
     @genre = genre
+    @@genres << genre
     @@count += 1
   end
   attr_accessor :name, :artist, :genre
